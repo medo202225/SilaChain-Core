65b054fc24dc2bc08f314fc2e2525ad6e99c980c
Ahmed Ibrahim <your-email@example.com>
2026-04-10 14:38:20 +0300

    Rename vmexec address helpers to Sila-native naming

--- internal/consensus/vmexec/executor.go
+++ internal/consensus/vmexec/executor.go
@@ -1390,11 +1390,11 @@ func contractWordFromAddress(address string) uint64 {
 	return 0
 }
 
-func legacyContractAddressFromWord(v uint64) string {
+func silaAddressFromWordLegacy(v uint64) string {
 	return fmt.Sprintf("contract%d", v)
 }
 
-func legacyContractWordFromAddress(address string) uint64 {
+func silaWordFromAddressLegacy(address string) uint64 {
 	var index uint64
 	n, _ := fmt.Sscanf(address, "contract%d", &index)
 	if n == 1 {
@@ -1403,14 +1403,14 @@ func legacyContractWordFromAddress(address string) uint64 {
 	return 0
 }
 
-func ethereumLikeAddressFromBytes(sum []byte) string {
+func silaAddressFromBytes(sum []byte) string {
 	if len(sum) < 20 {
 		return ""
 	}
 	return "0x" + fmt.Sprintf("%x", sum[len(sum)-20:])
 }
 
-func ethereumLikeCreateAddress(creator string, counter uint64) string {
+func silaCreateAddress(creator string, counter uint64) string {
 	hasher := sha3.NewLegacyKeccak256()
 	_, _ = hasher.Write([]byte(creator))
 
@@ -1418,10 +1418,10 @@ func ethereumLikeCreateAddress(creator string, counter uint64) string {
 	binary.BigEndian.PutUint64(counterBuf[:], counter)
 	_, _ = hasher.Write(counterBuf[:])
 
-	return ethereumLikeAddressFromBytes(hasher.Sum(nil))
+	return silaAddressFromBytes(hasher.Sum(nil))
 }
 
-func ethereumLikeCreate2Address(creator string, salt uint64, initCode []byte) string {
+func silaCreate2Address(creator string, salt uint64, initCode []byte) string {
 	hasher := sha3.NewLegacyKeccak256()
 	_, _ = hasher.Write([]byte{0xff})
 	_, _ = hasher.Write([]byte(creator))
@@ -1434,7 +1434,7 @@ func ethereumLikeCreate2Address(creator string, salt uint64, initCode []byte) st
 	_, _ = initHasher.Write(initCode)
 	_, _ = hasher.Write(initHasher.Sum(nil))
 
-	return ethereumLikeAddressFromBytes(hasher.Sum(nil))
+	return silaAddressFromBytes(hasher.Sum(nil))
 }
 func wordsForBytes(size uint64) uint64 {
 	if size == 0 {
