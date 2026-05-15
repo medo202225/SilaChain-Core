3463956a3690a5b7cb7d976b5de0b3da774fdeb8
AHMED IBRAHIM <ahmed@example.com>
2026-05-15 23:41:38 +0300

    cmd/sila: merge bootstrap and override helpers

--- cmd/sila/main.go
+++ cmd/sila/main.go
@@ -7,6 +7,8 @@ package main
 
 import (
 	"fmt"
+	ethconfig "github.com/sila-org/sila/eth/ethconfig"
+	"github.com/sila-org/sila/internal/version"
 	"os"
 	"slices"
 	"sort"
@@ -300,4 +302,47 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+// ApplyProtocolOverrides applies execution protocol override flags.
+func ApplyProtocolOverrides(ctx *cli.Context, cfg *ethconfig.Config) {
+	if ctx.IsSet(utils.OverrideOsaka.Name) {
+		v := ctx.Uint64(utils.OverrideOsaka.Name)
+		cfg.OverrideOsaka = &v
+	}
+	if ctx.IsSet(utils.OverrideBPO1.Name) {
+		v := ctx.Uint64(utils.OverrideBPO1.Name)
+		cfg.OverrideBPO1 = &v
+	}
+	if ctx.IsSet(utils.OverrideBPO2.Name) {
+		v := ctx.Uint64(utils.OverrideBPO2.Name)
+		cfg.OverrideBPO2 = &v
+	}
+	if ctx.IsSet(utils.OverrideUBT.Name) {
+		v := ctx.Uint64(utils.OverrideUBT.Name)
+		cfg.OverrideUBT = &v
+	}
+}
+
+var clientIdentifier = "sila"
+
+func SetClientIdentifier(name string) {
+	clientIdentifier = name
+}
+
+func ClientIdentifier() string {
+	return clientIdentifier
+}
+
+func DefaultNodeConfig() node.Config {
+	git, _ := version.VCS()
+
+	cfg := node.DefaultConfig
+	cfg.Name = clientIdentifier
+	cfg.Version = version.WithCommit(git.Commit, git.Date)
+	cfg.HTTPModules = append(cfg.HTTPModules, "eth")
+	cfg.WSModules = append(cfg.WSModules, "eth")
+	cfg.IPCPath = clientIdentifier + ".ipc"
+
+	return cfg
 }
