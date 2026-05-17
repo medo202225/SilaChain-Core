a5c48c8945dbab03c3565cbe1a421e38b7598c33
AHMED IBRAHIM <ahmed@example.com>
2026-05-17 16:48:37 +0300

    internal/silaapi: introduce block api boundary

---
+++ internal/silaapi/blockapi/blockapi.go
@@ -0,0 +1,8 @@
+// Copyright 2026 The SilaChain Authors
+// This file is part of the SilaChain library.
+
+package blockapi
+
+import ethapi "github.com/sila-org/sila/internal/ethapi"
+
+var RPCMarshalBlock = ethapi.RPCMarshalBlock
